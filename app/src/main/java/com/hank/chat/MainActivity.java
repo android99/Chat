a60ed4883ec4a47c8e976bade73dbf541f337f23
Hank Tom <hanktom@gmail.com>
2016-07-22 19:29:03 +0800

    Initial commit

---
+++ app/src/main/java/com/hank/chat/MainActivity.java
@@ -0,0 +1,33 @@
+package com.hank.chat;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    String name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText editText = new EditText(this);
+        if (name==null){
+            new AlertDialog.Builder(this)
+                    .setTitle("Name")
+                    .setView(editText)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            processToken();
+                        }
+                    }).show();
+
+        }
+    }
+
+    private void processToken() {
+        
+    }
+}
